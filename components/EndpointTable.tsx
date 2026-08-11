efa4f7d4b30c838d6691797be84129af64cc0fee
Tubagus Saifulloh <dutapanggulu@gmail.com>
2026-08-12 01:56:26 +0700

    update

--- components/EndpointTable.tsx
+++ components/EndpointTable.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
-import Link from "next/link";
-import MethodBadge from "./MethodBadge";
-import ImportOpenApiModal from "./ImportOpenApiModal";
 import type { MockEndpoint } from "@/lib/types";
+import Link from "next/link";
+import { useEffect, useMemo, useState } from "react";
+import ImportOpenApiModal from "./ImportOpenApiModal";
+import MethodBadge from "./MethodBadge";
 
 const METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"];
 const UNTAGGED = "__untagged__";
